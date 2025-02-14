764e265cead414414c5e472463110d7114a80209
venzy <david.venz@gmail.com>
2025-02-14 20:22:37 +1000

    Ch1 REPL L4 REPL

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if ! scanner.Scan() {
+			break
+		}
+		text := scanner.Text()
+		tokens := cleanInput(text)
+		if len(tokens) > 0 {
+			fmt.Printf("Your command was: %s\n", tokens[0])
+		}
+	}
 }
 
 func cleanInput(text string) []string {
